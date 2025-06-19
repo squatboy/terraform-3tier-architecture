a15d46ba3d0b54eaff7424a5acfeaef987a4a3f4
sqautboy <migonyoung01@gmail.com>
2025-06-19 13:21:51 +0900

    delete legacy

--- main.tf
+++
@@ -1,6 +0,0 @@
-// main.tf
-// This file is the main entry point for Terraform.
-// Resources are organized into separate .tf files for clarity.
-// Provider configuration is in versions.tf.
-// Variables are in variables.tf.
-// Outputs are in outputs.tf.
